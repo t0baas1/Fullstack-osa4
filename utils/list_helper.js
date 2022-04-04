508a6912f864ab23be1b86e86697fea2bce1fa48
Topi Kanninen <topi.kanninen@sci.fi>
2022-04-04 19:19:26 +0300

    osa 4 1/4 valmis

---
+++ utils/list_helper.js
@@ -0,0 +1,23 @@
+const { arrayBuffer } = require("stream/consumers")
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    let likes = blogs.map(blog => {
+        return blog.likes
+    })
+
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+    return blogs.length === 0
+        ? 0
+        : likes.reduce(reducer, 0)
+}
+
+module.exports = {
+    dummy,
+    totalLikes
+}
